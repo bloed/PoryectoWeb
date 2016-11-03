5bd9d9646085d374abe8ab0f2cea4974399433af
Joshua Mata <joshuamataa@gmail.com>
2016-11-03 16:18:54 -0600

    Info Acerca De

--- WebPage/resources/js/acercade.js
+++ WebPage/resources/js/acercade.js
@@ -1,5 +1,6 @@
 function onload(){
     getRequeriments();
+    getAcercaDe();
 }
 function getRequeriments() {
     var requirements = requestRequirements();
@@ -13,4 +14,12 @@ function getRequeriments() {
 }
 function requestRequirements(){
     return ['Tener celular para cada chofer de bus','Tener acceso a internet en el celular','Instalar la aplciación'];
+}
+
+function getAcercaDe(){
+    var element= document.getElementById("infoAcercaDe");
+    element.innerText = requestAcercaDe();
+}
+function requestAcercaDe(){
+    return "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed laoreet mi. Fusce posuere ultrices leo nec dictum. Cras tempus pretium quam mollis tempus. Suspendisse ornare mi vel velit mattis, vitae pharetra felis feugiat. Maecenas eros mauris, cursus ac elit eu, auctor pulvinar lacus. Curabitur et tincidunt ante. Nam arcu felis, rutrum at dignissim eu, commodo et sem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam ultricies tincidunt feugiat. Maecenas consectetur leo eget mi euismod, a molestie tortor blandit.";
 }
--- WebPage/resources/js/index.js
+++ WebPage/resources/js/index.js
@@ -25,4 +25,3 @@ function requestContent(id){
             return "JOSHUALorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed laoreet mi. Fusce posuere ultrices leo nec dictum. Cras tempus pretium quam mollis tempus. Suspendisse ornare mi vel velit mattis, vitae pharetra felis feugiat. Maecenas eros mauris, cursus ac elit eu, auctor pulvinar lacus. Curabitur et tincidunt ante. Nam arcu felis, rutrum at dignissim eu, commodo et sem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam ultricies tincidunt feugiat. Maecenas consectetur leo eget mi euismod, a molestie tortor blandit.";
     }
 }
-maininfoIndex
