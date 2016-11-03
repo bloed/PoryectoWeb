f983e1b37103985ea179f1523a3e8bd66d66a36c
Joshua Mata <joshuamataa@gmail.com>
2016-11-03 15:47:27 -0600

    Index js

--- WebPage/resources/js/index.js
+++ WebPage/resources/js/index.js
@@ -1,6 +1,9 @@
 var myIndex = 0;
-carousel();
 
+function onload(){
+    carousel();
+    loadMainInfo("maininfoIndex");
+}
 function carousel() {
     var i;
     var x = document.getElementsByClassName("mySlides");
@@ -12,3 +15,14 @@ function carousel() {
     x[myIndex-1].style.display = "block";
     setTimeout(carousel, 5000); // Change image every 2 seconds
 }
+function loadMainInfo(id){
+    var element= document.getElementById(id);
+    element.innerText = requestContent(id);
+}
+function requestContent(id){
+    switch (id){
+        case "maininfoIndex":
+            return "JOSHUALorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed laoreet mi. Fusce posuere ultrices leo nec dictum. Cras tempus pretium quam mollis tempus. Suspendisse ornare mi vel velit mattis, vitae pharetra felis feugiat. Maecenas eros mauris, cursus ac elit eu, auctor pulvinar lacus. Curabitur et tincidunt ante. Nam arcu felis, rutrum at dignissim eu, commodo et sem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam ultricies tincidunt feugiat. Maecenas consectetur leo eget mi euismod, a molestie tortor blandit.";
+    }
+}
+maininfoIndex
